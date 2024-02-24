445f9b738b2500f224d07242d83ab3e37b12f957
Faiz <faizkhan8225@gmail.com>
2024-02-24 13:31:07 +0530

    Add more folders and add auth structure

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express")
 
+const authRoutes = require("./routes/auth.routes")
+
 const app = express()
+
+app.use(authRoutes)
 
 app.listen(4000, () => {
     console.log("Server is running on port 4000")
---
+++ controllers/auth.controller.js
@@ -0,0 +1,16 @@
+exports.getSignup = (req, res, next) => {
+    res.render("auth/signup")
+}
+
+exports.postSignup = (req, res, next) => {
+
+}
+
+exports.getLogin = (req, res, next) => {
+    res.render("auth/login")
+}
+
+exports.postLogin = (req, res, next) => {
+    
+}
+
