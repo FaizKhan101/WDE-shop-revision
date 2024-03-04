29ffefe9d4bfbec8895c639c2b4bca1529e6cac5
Faiz <faizkhan8225@gmail.com>
2024-03-04 14:26:56 +0530

    Adding the image upload functionality.

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -5,3 +5,9 @@ exports.getProducts = (req, res, next) => {
 exports.getNewProduct = (req, res, next) => {
     res.render("admin/products/new-product")
 }
+
+exports.createProduct = (req, res, next) => {
+    console.log(req.body);
+    console.log(req.file);
+    res.redirect("/admin/products")
+}
--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,12 +1,15 @@
 const express = require("express")
 
 const adminController = require("../controllers/admin.controller")
+const configuredMulterMiddleware = require("../middlewares/image-upload")
 
 const router = express.Router()
 
 router.get("/products", adminController.getProducts)
 
 router.get("/products/new", adminController.getNewProduct)
+
+router.post("/products",configuredMulterMiddleware, adminController.createProduct)
 
 module.exports = router
 
